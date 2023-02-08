5946fcc2b60ccebb932ed4a8e0e9827be3a69c5d
yama-19 <a.yama19@outlook.com>
2023-02-08 10:48:15 +0900

    Change the dummy e-mail address

--- components/contact.js
+++ components/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
 
             {/* Contactにメールアドレスを設定。クリックするとメーラを起動する */}
             <h3 className={styles.heading}>Contact</h3>
-            <p><a className={styles.email} href="mailto:in_fo&#64;ex_ample.c0m">in_fo&#64;ex_ample.c0m</a></p>
+            <p><a className={styles.email} href="mailto:in_fo&#64;ex_ample.c0m">in-fo&#64;ex-ample.c0m</a></p>
 
             {/* ソーシャルアイコンを設定 */}
             <div className={styles.contactSocial}>
